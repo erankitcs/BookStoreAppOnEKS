ec4641ae6dbcf8eefa1e04be5f8bd8acf28a76ae
er.ankit.cs <er.ankit.cs@gmail.com>
2021-07-04 21:26:33 +1000

    eks cluster

---
+++ infrastructure/eks/eksctl/1-initial-cluster/command.ps1
@@ -0,0 +1,2 @@
+## Create cluster
+eksctl create cluster -f cluster.yaml
---
+++ infrastructure/eks/terraform/vpc-aws.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_region" "current" {}
+data "aws_availability_zones" "azs" {}
+
+locals {
+  cluster_name = "bookstore-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+#############################   VPC ###############################
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.2.0"
+
+  name                 = "bookstore-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.azs.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
