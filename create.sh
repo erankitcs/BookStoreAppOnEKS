e16550241c5c855ba3fc07e0482f29656977b216
er.ankit.cs <er.ankit.cs@gmail.com>
2021-07-22 23:09:36 +1000

    DNS update inputs.

--- create.sh
+++ create.sh
@@ -29,9 +29,22 @@ cd $curdir/infrastructure/eks/terraform
 terraform init -input=false
 #terraform plan -input=false
 terraform apply -input=false -auto-approve
+cluster_name=$(terraform output -raw cluster_name)
 echo "*** Step- 3 - EKS Tooling-External DNS,Ingress Controller etc ***"
 echo "----------Creating Namespaces"
 cd $curdir/infrastructure/k8s-tooling/namespaces
 kubectl apply -f development.yaml
 kubectl apply -f prod.yaml
 echo "----------Creating External DNS"
+cd $curdir/infrastructure/k8s-tooling/external-dns
+kubectl apply -f external-dns.yaml
+echo "----------Creating ALB Controller."
+cd $curdir/infrastructure/k8s-tooling/alb-controller
+template=`cat "alb-ingress-controller.yaml.template" | sed "s/{{CLUSTER_NAME}}/$cluster_name/g"`
+echo "$template" | kubectl apply -f -
+echo "************** Step- 4 - Creating CI/CD pipeline ****************"
+cd $curdir/infrastructure/cicd/terraform
+terraform init -input=false
+#terraform plan -input=false
+terraform apply -input=false -auto-approve
+echo "************** Step- 5 - Pushing code into Code Commits ****************"
